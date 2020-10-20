a0420f3296d9efdf0f7c66de78a317c51a59d122
jzerez <Jonathan.zerez@students.olin.edu>
2020-10-21 01:04:27 +0800

    working on readme writeup

--- README.md
+++ README.md
@@ -1,31 +1,76 @@
 # robot_localization
-This is the base repo for the Olin Computational Robotics Robot Localization project
+*ENGR3590: A Computational Introduction to Robotics, Olin College of Engineering, FA2020*
 
-## Implementation Plan
-### Key Steps
-1. Random Sampling of `n` points throughout entire map
-2. Apply motion transform to the `n` sampled points based on data from `\odom`
-3. Look at sensor data
-4. Calculate the probability that each point matches the sensor data recorded
-5. Re-sample points based on probability (probability of resampling is equal to: 1 - probability of location)
-  * Consider other criteria for which points require re-sampling (re-sample least x% probable? re-sample all points whose probability is less than y%? )
-  * Determine criteria for the location of re-sampled points. Does it follow some distribution around the likely points? What is the variance of that distribution?
-6. Update robot pose
-  * Determine criteria for doing this (pick the highest probability point? Randomly sample from probability field?)
-  * Stretch goal: implement some means
+*Nathan Estill, Jonathan Zerez*
 
-### Testing:
-1. Make a function that spawns `n` particles following some distribution
-  * Plot them in RVIZ and validate by eye
-2. Make a function transforms the points based on odom information + some noise
-  * Plot them in RVIZ and validate by eye
-3. Make a function that takes "would be" lidar scans at the location and orientation of each of the `n` particles
-  * Teleport NEATO to the same location and see if the lidar points match up in RVIZ
-4. Make a function that calculates the probability associated with each particle
-  * Plot them in RVIZ with color or size proportional to probability
-  * Qualitatively verify that probability seems reasonable
-5. Make a function that updates the robot's pose according to the weighted particles
-  * Plot in RVIZ and make sure new robot pose is in the right spot and that a reasonable spot is chosen
+This is the base repo for the Olin Computational Robotics Robot Localization project. The goal of the project was to implement a particle filter in order to localize a robot within a known map.
 
-### Logistics:
-We plan to do most of the code from scratch.
+This project's robotic platform is the NEATO vacuum robot. The NEATO is equipped with a number of basic sensors including wheel encoders, a small array of bump sensors, and a 2D LIDAR unit. Below is an image of a NEATO. The blue circle atop the unit is the LIDAR sensor.
+
+![NEATO](./assets/NEATO.jpg)
+
+## Running the Code
+The code can be run in two ways, to run the code on a prerecorded bag file, paste the following into your command line once you've cloned the repo:
+
+`roslaunch robot_localizer test_bagfile.launch map_name:=ac109_1`
+
+To run the particle filter with the goal of localizing the NEATO in real time, paste the following command into your command line:
+
+`roslaunch robot_localizer test_live.launch map_name:=ac109_1`
+
+In both cases, the map name can be changed to any of the file names found the `/maps` directory.
+
+Once the rviz window opens, simply provide a 2D pose estimation to initialize the particles, and you're good to go! Note, the initial pose estimation needs to be pretty good in order for the particle filter to work well.
+
+## Architecture and Implementation
+In order to complete this project, we need to find the transform that relates the odometry frame to the global map frame. We know that there is a certain amount of noise and imprecision in the NEATO's wheel encoders, so we know that the odometry frame is almost guaranteed to drift away from the global frame over time. This is where the particle filter comes in. The particle filter uses data from the NEATO's LIDAR sensor in order to periodically correct the deviation of the `/odom` frame from the map frame.
+
+![Transforms](./assets/transform_diagram.png)
+
+The particle filter works by first initializing a set of particles based on the provided initial 2D pose estimation. Each particle has three attributes, an x position in meters, a y position in meters, and an orientation in radians. Once the NEATO starts moving, we translate each point based on readings from the encoder. We then use the LIDAR to calculate the probability that any given particle represents the true position of the robot. Finally, we re-sample the particles based on their probabilities.
+
+
+### Initializing the particles
+For our initial set of particles, we get a initial pose estimate from clicking in Rviz. We take this pose estimate and create particles that are normally distributed around this point. The particles are also normally distributed around the orientation of the initial pose estimate.
+
+![Initial Particles](./assets/InitialParticles.png)
+
+### Updating the `/odom` frame from encoders
+To ensure that the particles keep up with the robots movement, we transform them based on the '/odom' topic. One major design decision we made was in how we handled this update. One path forward was to calculate the change relative to the NEATO's orientation. For example, if the NEATO moved directly forward relative to its own heading, the particles would also move directly forward relative to their own heading. The other path was to calculate the change relative to the position overall. For example, if the NEATO moves 1 meter in the x direction and 0.5 meters in the y direction, the particles would also move 1 meter in the x direction and 0.5 meters in the y direction. We ended up choosing the latter because it was much simpler to implement and the additional accuracy would not have that great of an impact because the particles are mostly facing the same direction. So for each time step, we calculate the difference between the previous odom pose and the current odom pose and apply that difference to each of our particles. Additionally, each particle also moves by a certain amount of random noise that is proportional to the amount that it moved in both translational and rotational amounts.
+
+### Transforming the LIDAR scan to each particle's reference frame and then to the map frame
+Because the LIDAR scan is given as a distance and degree, it has to be transferred to Cartesian coordinates. It also has to be in the map's frame so that we can later compare the points to the points in the map. For each particle, we first add the particles orientation to the LIDAR's points. Then we convert the resulting polar coordinates to Cartesian, which results in a set of points localized around each particle. We then add the particle's x and y locations to get each point in the map's frame of reference.
+
+### Calculating the weight of each particle
+Once the LIDAR scan points are transformed into the local coordinate system for a given particle, we then move on to calculate the so called "weight" of the particle, which essentially a measure of how good of a guess a given particle is. The better a particle's position and orientation explain the LIDAR data, the higher its weight will be.
+
+In order to calculate the weight of a particle, we first need to implement our sensor model. In our case, we assume that the LIDAR sensor returns distance values that follow a normal distribution with a standard deviation of 2 cm from the mean. For each LIDAR point in the particle's frame, we calculate the nearest point in the map. We then assign a probability to each point by picking the corresponding probability* from our distribution function given the distance to the nearest point.
+
+
+
+### Resampling the particles based on weights
+After calculating the weight's of each particle, we want to remove some particles that are poor estimate's of the robot's pose, and resample them. To do this, we define a weight threshold. If a particle's weight is above the threshold, it remains unchanged, but if it's weight is less than the threshold, it's position changes to match one of the particle's whose weight was above the threshold. The higher a particle's weight, the higher chance that a poor particle will change to match that particle's position. The particles exactly match the assimilated particle, as noise is applied in the odometry transforms.
+
+## Design Decisions
+One design decision that we made was to make the odometry applied to the particles noisy as opposed to the resampling. We did this so that larger changes in movement would result in larger spreads of the particle. This aligns with the idea that the more the robot moves, the higher the odometry will be off from the ground truth.
+
+## Challenges
+Plotting in Rviz in such a way that conveys the information we wanted was a big challenge. Initially, we plotted Marker points for the particles, but those markers are sometimes too big. When we wanted to communicate the weights of each particle, scaled the length of each arrow by the weight. However, it gets harder to determine the exact locations when so many arrows are overlapping. I don't think we found a winning visualization aside from changing the form based on what property we wanted to single out.
+
+## Important Learning Takeaways
+
+Having a well established visualization method is crucial to being able to effectively debug robotics code. Seeing each of the arrows in Rviz really helped us find and remove mistakes that we had made that would have taken a lot longer if we had manually gone through and tried to find inconsistencies.
+
+## Prompt
+
+   How did you solve the problem? (Note: this doesn’t have to be super-detailed, you should try to explain what you did at a high-level so that others in the class could reasonably understand what you did).
+
+
+  Describe a design decision you had to make when working on your project and what you ultimately did (and why)? These design decisions could be particular choices for how you implemented some part of an algorithm or perhaps a decision regarding which of two external packages to use in your project.
+
+
+  What if any challenges did you face along the way?
+  What would you do to improve your project if you had more time?
+
+
+  Did you learn any interesting lessons for future robotic programming projects? These could relate to working on robotics projects in teams, working on more open-ended (and longer term) problems, or any other relevant topic.
--- robot_localizer/scripts/main.py
+++ robot_localizer/scripts/main.py
@@ -25,37 +25,70 @@ from tf import TransformListener
 class ParticleFilter():
 
     def __init__(self):
-        #
+        # Initialize node and attributes
         rospy.init_node("ParticleFilter")
+
+        # Subscribers
         self.lidar_sub = rospy.Subscriber("/scan",LaserScan, self.lidar_callback)
         self.odom_sub = rospy.Subscriber("/odom",Odometry, self.odom_callback)
+        self.initial_estimate_sub = rospy.Subscriber("/initialpose",PoseWithCovarianceStamped,self.pose_estimate_callback)
+
+        # publishers
         self.all_particles_pub = rospy.Publisher("/visualization_particles", MarkerArray, queue_size=10)
         self.init_particles_pub = rospy.Publisher("/visualization_init",MarkerArray,queue_size=10)
-        self.initial_estimate_sub = rospy.Subscriber("/initialpose",PoseWithCovarianceStamped,self.pose_estimate_callback)
         self.new_particles_pub = rospy.Publisher("/particlecloud",PoseArray,queue_size=10)
 
-        self.occupancy_field = OccupancyField()
+        # constants
         self.number_of_particles = 30
-        self.particles = np.ones([3,self.number_of_particles], dtype=np.float)
-        self.weights = np.ones(self.number_of_particles, dtype=np.float)
         pos_std_dev = 0.25
         ori_std_dev = 25 * math.pi / 180
         self.initial_std_dev = np.array([[pos_std_dev, pos_std_dev, ori_std_dev]]).T
-        self.odom_tf_time = 0
-        self.base_tf_time = 0
         self.lidar_std_dev = 0.02
         self.resample_threshold = 0.1
+
+        # changing attributes
+        self.particles = np.ones([3,self.number_of_particles], dtype=np.float)
+        self.weights = np.ones(self.number_of_particles, dtype=np.float)
+        self.odom_tf_time = 0
+        self.base_tf_time = 0
         self.scan = None
         self.prev_pose = None
         self.delta_pose = None
         self.initial_pose_estimate = None
         self.pose = None
+
+        # helper classes
+        self.occupancy_field = OccupancyField()
         self.tf_listener = TransformListener()
         self.tf_broadcaster = TransformBroadcaster()
-
         self.transform_helper = TFHelper()
 
         rospy.loginfo("Initialized")
+
+    def lidar_callback(self,msg):
+        # Lidar Subscriber callback function.
+        self.scan = msg.ranges;
+
+    def odom_callback(self,msg):
+        # Odometry Subscriber callback function.
+        x = msg.pose.pose.position.x
+        y = msg.pose.pose.position.y
+        t = euler_from_quaternion([msg.pose.pose.orientation.w,msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,msg.pose.pose.orientation.z])[0]
+        pose = -np.array([[x,y,t]]).T
+        if self.prev_pose is None:
+            self.prev_pose = pose
+        self.pose = pose
+
+        self.apply_odom_transform()
+        self.plot_particles(self.particles)
+        self.transform_helper.send_last_map_to_odom_transform()
+
+    def pose_estimate_callback(self,msg):
+        # Rviz pose estimate callback function.
+        position = msg.pose.pose.position
+        orientation = euler_from_quaternion([msg.pose.pose.orientation.w,msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,msg.pose.pose.orientation.z])[0]
+        pose = [position.x,position.y,-orientation - math.pi]
+        self.initial_pose_estimate = np.array([pose]).T
 
     def sample_points(self, mean, std):
         # samples a uniform distribution of points
@@ -66,12 +99,11 @@ class ParticleFilter():
         # Takes the weights of each particle and resamples them according to that weight
         replace = self.weights < self.resample_threshold
         kept_weights = np.multiply(replace,self.weights)
-        # kept_inds = np.arange(kept_weights.size)[~replace]
         probs = kept_weights / sum(kept_weights)
         replace_inds = np.arange(self.weights.size)[replace]
 
         if kept_weights.size == 0:
-            rospy.logerr("No weights")
+            rospy.logerr("No particles meet threshold")
         else:
             for i in replace_inds:
                 choice = np.random.choice(kept_weights.size, p=probs)
@@ -84,46 +116,11 @@ class ParticleFilter():
         self.delta_pose = self.pose - self.prev_pose
         self.prev_pose = self.pose
 
-        if abs(self.delta_pose[2]) < 0.5:
+        if abs(self.delta_pose[2]) < 0.5: # this was added to get rid of the orientation wrap around effect
             delta_std = abs(self.delta_pose) * np.transpose([[1.5, 1.5, 0.9]])
             noisy_deltas = self.sample_points(self.delta_pose, delta_std)
 
             self.particles = self.particles + noisy_deltas
-
-
-
-    def lidar_callback(self,msg):
-        # Lidar Subscriber callback function.
-        self.scan = msg.ranges;
-
-    def odom_callback(self,msg):
-        x = msg.pose.pose.position.x
-        y = msg.pose.pose.position.y
-        t = euler_from_quaternion([msg.pose.pose.orientation.w,msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,msg.pose.pose.orientation.z])[0]
-        pose = -np.array([[x,y,t]]).T
-        if self.prev_pose is None:
-            self.prev_pose = pose
-            self.pose = pose
-        else:
-            self.pose = pose
-
-        self.apply_odom_transform()
-        self.plot_particles_new(self.particles)
-        # self.plot_particles(self.particles, ColorRGBA(1, 0, 0.5, 0.5), self.all_particles_pub)
-
-        # avg_pose = self.calc_avg_particle()
-        # self.update_transform(avg_pose)
-        # self.update_transform(avg_pose, target_frame='odom')
-        self.transform_helper.send_last_map_to_odom_transform()
-
-    def pose_estimate_callback(self,msg):
-        rospy.logdebug("Callback Good")
-        position = msg.pose.pose.position
-                # best_pose = self.particles[:, max_weight_ind]
-                # self.update_transform(best_pose,'odom')
-        orientation = euler_from_quaternion([msg.pose.pose.orientation.w,msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,msg.pose.pose.orientation.z])[0]
-        pose = [position.x,position.y,-orientation - math.pi]
-        self.initial_pose_estimate = np.array([pose]).T
 
     def calc_prob(self):
         # Reweight particles based on compatibility with laser scan
@@ -140,10 +137,10 @@ class ParticleFilter():
                 dist = self.occupancy_field.get_closest_obstacle_distance(x,y)
                 prob = norm(0, self.lidar_std_dev).pdf(dist) / (0.4 / self.lidar_std_dev)
                 weight_sum += prob**3
-            self.weights[i] = weight_sum / 361
+            self.weights[i] = weight_sum / len(lidar_x[::2])
 
     def update_transform(self, pose, target_frame='base_laser_link'):
-        # Updates the transform between the map frame and the odom frame
+        # Currently unused, Updates the transform between the map frame and the odom frame
         if((rospy.get_rostime() != self.odom_tf_time and target_frame == 'odom') or (rospy.get_rostime() != self.base_tf_time and target_frame == 'base_laser_link')):
             self.tf_broadcaster.sendTransform((pose[0], pose[1], 0),
                               quaternion_from_euler(0,0,pose[2]+math.pi),
@@ -166,27 +163,8 @@ class ParticleFilter():
 
         return xs, ys
 
-    def plot_particles(self, particles, color, pub):
-        marker_array = MarkerArray()
-        for i, particle in enumerate(particles.T):
-            w=self.weights[i] * 10
-            nextMarker = Marker()
-            x = particle[0]
-            y = particle[1]
-            t = particle[2]
-            nextMarker.header = Header(stamp = rospy.Time.now(), frame_id="map")
-            nextMarker.id = i;
-            nextMarker.ns="particle"
-            nextMarker.type = Marker.ARROW
-            nextMarker.points = [Point(x,y,0), Point(x+math.cos(t)*w, y+math.sin(t)*w, 0)]
-            # nextMarker.pose = Pose(Point(x,y,0), Quaternion(0,0,0,0))
-            nextMarker.scale = Vector3(0.1,0.3,0.2)
-            nextMarker.color = color
-            nextMarker.action = Marker.ADD
-            marker_array.markers.append(nextMarker)
-        pub.publish(marker_array)
-
-    def plot_particles_new(self,particles):
+    def plot_particles(self,particles):
+        # plots the particles in the pose array particle cloud topic
         pose_array = PoseArray()
         pose_array.header = Header(stamp = rospy.Time.now(),frame_id="map")
         for i,particle in enumerate(particles.T):
@@ -197,39 +175,27 @@ class ParticleFilter():
             pose_array.poses.append(nextPose)
         self.new_particles_pub.publish(pose_array)
 
-
     def calc_avg_particle(self):
+        # calculates the weighted average position of the particles
         return np.sum(self.particles * self.weights, axis=1) / sum(self.weights)
 
     def main(self):
         r = rospy.Rate(5)
 
         while(not rospy.is_shutdown() and (self.scan is None or self.initial_pose_estimate is None or self.pose is None)):
-            rospy.logwarn("Still Missing Something")
+            rospy.logwarn("One of the necessary components has not yet been initialized")
             r.sleep()
 
         self.particles = self.sample_points(self.initial_pose_estimate, self.initial_std_dev)
-        counter = 0
         while(not rospy.is_shutdown()):
-            if counter % 2 == 0:
-                rospy.loginfo("Calculating Probability")
-                self.calc_prob()
-                # max_weight_ind = np.argmax(self.weights)
-                # best_pose = self.particles[:, max_weight_ind]
-                # self.update_transform(best_pose,'odom')
-                best_pose = self.calc_avg_particle()
-                robot_pose = Pose(position=Point(x=best_pose[0],y=best_pose[1]),orientation=Quaternion(*quaternion_from_euler(0,0,best_pose[2])))
-                self.transform_helper.fix_map_to_odom_transform(robot_pose,rospy.Time.now())
-                self.resample_points()
-
-            counter += 1
+            self.calc_prob()
+            best_pose = self.calc_avg_particle()
+            robot_pose = Pose(position=Point(x=best_pose[0],y=best_pose[1]),orientation=Quaternion(*quaternion_from_euler(0,0,best_pose[2])))
+            self.transform_helper.fix_map_to_odom_transform(robot_pose,rospy.Time.now())
+            self.resample_points()
             r.sleep()
-            # self.delta_pose = (0,0,0)
 
 
 if __name__ == '__main__':
     PF = ParticleFilter()
     PF.main()
-    # PF.initial_resample_points(np.array([[1,1,1]]).T)
-    # print(PF.particles)
-    # print(np.std(PF.particles, axis=1))
